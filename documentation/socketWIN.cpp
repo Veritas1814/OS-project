181501d1b8f27f2ab48464a09869d4224e38a711
Veritas1814 <matviyshumylovych@gmail.com>
2025-12-13 12:44:31 +0200

    stupid test example cmake

--- documentation/socketWIN.cpp
+++ documentation/socketWIN.cpp
@@ -1,29 +1,22 @@
-#include <Process.h>
+#include <Process.h>        
 #include <iostream>
 #include <string>
 
 int main(int argc, char* argv[]) {
     // --- CHILD LOGIC ---
     if (argc > 1) {
-        // Process.cpp passes: [1]=type, [2]=stdinPort, [3]=stdoutPort, [4]=stderrPort
         unsigned short inPort  = static_cast<unsigned short>(std::stoi(argv[2]));
         unsigned short outPort = static_cast<unsigned short>(std::stoi(argv[3]));
 
         SocketChannel chIn, chOut;
 
-        // Child acts as Client. Connects to Parent.
-        // Parent's Stdin is where Child READS from.
-        chIn.create(SocketType::Tcp);
+        chIn.create(SocketType::IPv4); 
         if (!chIn.connectTo("127.0.0.1", inPort)) return 1;
 
-        // Parent's Stdout is where Child WRITES to.
-        chOut.create(SocketType::Tcp);
+        chOut.create(SocketType::IPv4);
         if (!chOut.connectTo("127.0.0.1", outPort)) return 1;
 
-        // Read from Parent
         std::string msg = chIn.readAll();
-
-        // Write to Parent
         std::string reply = "Socket Msg: " + msg;
         chOut.write(reply);
 
@@ -36,17 +29,11 @@ int main(int argc, char* argv[]) {
     Process p(argv[0], {});
 
     try {
-        // Parent acts as Server.
-        // Use port 8000 (Stdin), 8001 (Stdout), 8002 (Stderr)
-        p.startSockets(8000, SocketType::Tcp);
+        p.startSockets(8000, SocketType::IPv4);
 
-        // writeStdin sends data via the first socket
         p.writeStdin("Hello via TCP");
-
-        // Closes the first socket (sending EOF to child) so child's readAll() returns
         p.closeStdin();
 
-        // readStdout reads from the second socket
         std::string response = p.readStdout();
 
         p.wait();
